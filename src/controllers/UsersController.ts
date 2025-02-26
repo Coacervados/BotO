c07fa2f05c49a5767a6a33fa2ecd92076717c430
Matusal3m <matusalemdedeus@gmail.com>
2025-02-26 14:44:26 -0300

    add users controller

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from 'express';
+import { Controller, Get, Patch } from '../../packages';
+import { UsersService } from '../services';
+
+@Controller('/users')
+export class UsersController {
+    private readonly service = new UsersService();
+
+    @Get()
+    async getAll(req: Request, res: Response) {
+        const users = await this.service.all();
+
+        res.status(200).send(users);
+    }
+
+    @Get('/:id')
+    async getById(req: Request, res: Response) {
+        const id = parseInt(req.params.id);
+
+        if (isNaN(id)) {
+            res.status(401).send('Invalid Id');
+            return;
+        }
+
+        const users = await this.service.findById(id);
+
+        res.status(200).send(users);
+    }
+
+    @Patch('/:id')
+    async updateUser(req: Request, res: Response) {
+        const id = parseInt(req.params.id);
+
+        if (isNaN(id)) {
+            res.status(401).send('Insert a valid Id');
+            return;
+        }
+
+        const users = await this.service.findById(id);
+
+        if (!users) {
+            res.status(404).send('USer not found');
+        }
+
+        res.status(200).send(users);
+    }
+}
