1ba359f3dd7be06fb4976c4ff97543507e7229bf
Isaac Sales <124395470+IsaacSales15@users.noreply.github.com>
2025-02-26 12:32:07 -0300

    gemini service add

--- src/services/GeminiService.ts
+++ src/services/GeminiService.ts
@@ -1,10 +1,23 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAI } from '@google/generative-ai';
+import dotenv from 'dotenv';
 
-export class GeminiService{
-  private apiKey = new GoogleGenerativeAI(process.env.API_KEY!);
+dotenv.config();
 
-  constructor(apiKey: GoogleGenerativeAI) {
-    this.apiKey = apiKey;
-  }
+export class GeminiService {
+    private model: any;
 
+    constructor() {
+        const ia = new GoogleGenerativeAI(process.env.GEMINI_KEY!);
+        this.model = ia.getGenerativeModel({
+            model: 'gemini-2.0-flash-thinking-exp-01-21',
+        });
+    }
+
+    async chat(history: string[]): Promise<string> {
+        const context = history.join();
+        const result = await this.model.generativeContext(context);
+        return result.response.text();
+    }
 }
+
+export default new GeminiService();
