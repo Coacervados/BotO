d9fe5f96d110812580b96284c39348db71e1acd2
IsaacSales15 <isaacrodrigues1516@gmail.com>
2025-02-27 20:24:05 -0300

    commit test

--- src/services/GeminiService.ts
+++ src/services/GeminiService.ts
@@ -1,7 +1,10 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
+import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+const prisma = new PrismaClient();
 
 export class GeminiService {
     private model: any;
@@ -13,8 +16,14 @@ export class GeminiService {
         });
     }
 
-    async chat(history: string[]): Promise<string> {
-        const context = history.join();
+    async chat(userId: number, custumerId: number, message: string): Promise<string> {
+	
+	const history = await prisma.chat.findFirst({
+		where: {userId, customerId},
+		include: {sender: true}
+	});
+
+        const context = history ? `${history.sender.name}: ${message}`:`User: ${message}`; 
         const result = await this.model.generativeContext(context);
         return result.response.text();
     }
