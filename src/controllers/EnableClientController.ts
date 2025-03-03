0d92770f6f019b9ed7ceff1127d9ccac52996432
Matusal3m <matusalemdedeus@gmail.com>
2025-03-03 20:36:19 -0300

    bug fixes

--- src/controllers/EnableClientController.ts
+++ src/controllers/EnableClientController.ts
@@ -1,15 +1,15 @@
-import { Controller, GlobalMiddleware, Post } from 'damex';
+import { Controller, Middleware, Post } from 'damex';
 import { Request, Response } from 'express';
-import { AuthMiddleware } from '../middlewares/AuthMiddleware';
+import { AuthMiddleware } from '../middlewares';
 import { WhatsappUsersService } from '../services';
 
 @Controller('/enable-client')
-@GlobalMiddleware([AuthMiddleware])
 export class EnableClientController {
     @Post('/whatsapp')
+    @Middleware([AuthMiddleware])
     async enableWhatsapp(req: Request, res: Response) {
         const { phoneNumber } = req.body as { phoneNumber: string };
-        const { id, email } = req.user;
+        const { id, email } = res.locals.user;
 
         if (!phoneNumber) {
             res.status(400).send('phoneNumber is required');
