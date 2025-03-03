81c339b13ac139d00d11fecc495e319fc5510f72
IsaacSales15 <isaacrodrigues1516@gmail.com>
2025-03-02 22:59:17 -0300

    continuation of InstagramService

--- src/services/InstagramService.ts
+++ src/services/InstagramService.ts
@@ -1,6 +1,8 @@
-import { IgApiClient } from 'instagram-private-api';
-import { prisma } from '../libs';
+import { DirectInboxFeed, IgApiClient } from 'instagram-private-api';
 import dotenv from 'dotenv';
+import { threadId } from 'worker_threads';
+import GeminiService  from './GeminiService';
+import { prisma } from '../libs';
 
 dotenv.config();
 
@@ -17,9 +19,63 @@ export class InstagramService {
 	}
 
 	async inbox() {
+		const inboxMsg = this.ig.feed.directInbox();
+		const threads = await inboxMsg.items();
 
+		for(const thread of threads) {
+			const threadId = thread.thread_id;
+
+			for(const item of thread.items) {
+				if(item.item_type === 'text' && String(item.user_id) !== this.ig.state.cookieUserId) {
+					const senderId = item.user_id.toString();
+					const text = item.text;
+
+					console.log(`Origem da mensagem ${senderId}: ${text}`);
+
+					await this.processMsg(senderId, text!, thread.thread_id);
+				}
+			}
+		}
+	}
+
+	async processMsg(senderId: string, text: string, threadId: string){
+		let customer = await prisma.customer.findFirst({
+			where: { 
+				ig: senderId
+			}
+		});
+
+		// Preciso de uma lógica para achar o dono do  omnichannel correspondente
+		const omnichannel = await prisma.omniChannel.findFirst();
+
+		if (!omnichannel) {
+			throw new Error("Sem omnichannell disponível");
+		}
+
+		if(!customer) {
+			customer = await prisma.customer.create({
+				data: {
+					name: `InstagramUser_${senderId}`,
+					ig: senderId,
+					sendFrom: 'instagram',
+					// essa parte sofrerá alterações, para aplicação de uma mais coerente
+					userId: omnichannel.userId
+				}
+			});
+		}
+
+		const res = await GeminiService.chat(customer.userId, customer.id, text);
+
+		await this
+	}
+
+	async sendMsg(threadId: string, msg: string) {
+		const thread = this.ig.entity.directThread(threadId);
+		await thread.broadcastText(msg);
+
+		console.log(`Mensagem enviada: ${msg}`);
 	}
 }
 
-
+export default new InstagramService();
 
